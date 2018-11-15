09a73e481db7c0f2d7c648f865cb5bb7b03af4a9
Robert K. Bell <robert.bell@envoyat.com>
2018-11-16 10:23:47 +1100

    testing commit signing from ide

--- web/src/index.ts
+++ web/src/index.ts
@@ -6,7 +6,7 @@ app.use(serve('./static'))
 
 app.use(ctx => {
   console.info(`Page requested, url: '${ctx.request.url}'`)
-  ctx.body = 'Hello from Koa'
+  ctx.body = 'Hello Koa'
 })
 
 app.listen(3000)
