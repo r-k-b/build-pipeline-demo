aa9691db180e085e4441d01c890c0f326ba4447c
Robert K. Bell <robert.bell@envoyat.com>
2018-11-15 14:47:44 +1100

    feat: print something to stdout

--- web/index.js
+++ web/index.js
@@ -3,7 +3,10 @@ const app = new Koa()
 
 // response
 app.use(ctx => {
+  console.info(`Page requested, url: '${ctx.request.url}'`)
   ctx.body = 'Hello Koa'
 })
 
 app.listen(3000)
+
+console.info('App listening on port 3000.')
